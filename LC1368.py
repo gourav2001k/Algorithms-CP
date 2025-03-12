ca93272eb701e94ef14fc34d7dc88b0af8196037
Gourav Singh <58901846+gourav2001k@users.noreply.github.com>
2025-03-12 13:02:02 +0530

    LC1368 Approach#2

--- LC1368.py
+++ LC1368.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/description/
 
+# Approach 1: Priority Queue
+# TC: O(N*M*Log(N*M))
 class Solution:
     def minCost(self, mat: List[List[int]]) -> int:
         INF = 10**9
@@ -30,4 +32,42 @@ class Solution:
                     continue
                 dp[a][b] = cc
                 heappush(pq, (cc, a, b))
+        return -1
+
+
+# Approach 2: Replacing Priority Queue with Deque
+# TC: O(N*M)
+class Solution:
+    def minCost(self, mat: List[List[int]]) -> int:
+        INF = 10**9
+        n, m = len(mat), len(mat[0])
+        dp = [[INF for j in range(m)]for i in range(n)]
+
+        def valid(x, y):
+            return 0 <= x < n and 0 <= y < m
+
+        dp[0][0] = 0
+        dq = deque([(0, 0, 0)])
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        while dq:
+            c, x, y = dq.popleft()
+            if x == n-1 and y == m-1:
+                return c
+            if dp[x][y] < c:
+                continue
+            for i in range(4):
+                a, b = x+dirs[i][0], y+dirs[i][1]
+                if not valid(a, b):
+                    continue
+                if mat[x][y] == i+1:
+                    cc = c
+                else:
+                    cc = c+1
+                if dp[a][b] <= cc:
+                    continue
+                dp[a][b] = cc
+                if cc == c:
+                    dq.appendleft((cc, a, b))
+                else:
+                    dq.append((cc, a, b))
         return -1
