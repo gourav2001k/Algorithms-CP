a9f0d7000d76331a3c36e93705e3847640048d42
Gourav Singh <58901846+gourav2001k@users.noreply.github.com>
2025-04-01 20:46:19 +0530

    LC-Daily 01/04/25 Better coding practices

--- LC-Daily/LC2140.py
+++ LC-Daily/LC2140.py
@@ -3,11 +3,9 @@
 class Solution:
     def mostPoints(self, arr: List[List[int]]) -> int:
         n = len(arr)
-        dp = [0 for i in range(n)]
+        dp = [0 for i in range(n+1)]
         for i in range(n-1, -1, -1):
-            dp[i] = arr[i][0]
-            if arr[i][1]+i+1 < n:
-                dp[i] += dp[arr[i][1]+i+1]
-            if i+1 < n:
-                dp[i] = max(dp[i], dp[i+1])
+            pts, brainPwr = arr[i]
+            dp[i] = pts+dp[min(brainPwr+i+1, n)]
+            dp[i] = max(dp[i], dp[i+1])
         return dp[0]
