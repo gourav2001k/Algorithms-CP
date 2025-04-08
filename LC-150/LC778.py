8cf68be82a000055b3fe3566bd8e80d35f6d2748
Gourav Singh <58901846+gourav2001k@users.noreply.github.com>
2025-04-08 18:07:18 +0530

    LC#778-Hard Approach#2

--- LC-150/LC778.py
+++ LC-150/LC778.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/swim-in-rising-water/description/
 
+# Approach 1: Using Binary Search TC: O(N*M*Log(N*M))
 class Solution:
     def swimInWater(self, mat: List[List[int]]) -> int:
         n = len(mat)
@@ -37,3 +38,30 @@ class Solution:
             else:
                 l = m
         return r
+
+
+# Approach 2: Using Heap/Dijkstra TC: O(N*M*Log(N*M))
+
+class Solution:
+    def swimInWater(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+
+        dirs = [0, 1, 0, -1, 0]
+        def verify(x, y): return 0 <= x < n and 0 <= y < n
+
+        pq = [(mat[0][0], 0, 0)]
+        vis = [[0 for i in range(n)]for j in range(n)]
+        vis[0][0] = 1
+        while pq:
+            cost, x, y = heappop(pq)
+            if x == n-1 and y == n-1:
+                return cost
+            for i in range(4):
+                a, b = x+dirs[i], y+dirs[i+1]
+                if not verify(a, b):
+                    continue
+                if vis[a][b]:
+                    continue
+                vis[a][b] = 1
+                heappush(pq, (max(mat[a][b], cost), a, b))
+        return -1
