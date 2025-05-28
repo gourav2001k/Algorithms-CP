4ff5d0b56ef396fb4aa279e07a6a4633dd77f84d
Gourav Singh <58901846+gourav2001k@users.noreply.github.com>
2025-05-29 00:21:32 +0530

    LC-Daily 29/05/25 Approach#2

--- LC-Daily/LC3373.py
+++ LC-Daily/LC3373.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-ii/description
 
+# Slightly Complex
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
         n, m = len(edges1)+1, len(edges2)+1
@@ -54,3 +55,42 @@ class Solution:
             if cur == par:
                 continue
             self.constructTarget(cur, root, counts, target, g)
+
+
+# Approach 2: Simplified using Bipartite
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
+        n, m = len(edges1)+1, len(edges2)+1
+        g1 = defaultdict(list)
+        g2 = defaultdict(list)
+        for a, b in edges1:
+            g1[a].append(b)
+            g1[b].append(a)
+
+        for a, b in edges2:
+            g2[a].append(b)
+            g2[b].append(a)
+
+        colors1 = [0 for i in range(n)]
+        colors2 = [0 for i in range(m)]
+        odd, even = self.oddEvenCount(0, -1, 0, colors1, g1)
+        maxReach = max(self.oddEvenCount(0, -1, 0, colors2, g2))
+
+        out = [maxReach for i in range(n)]
+        for i in range(n):
+            if colors1[i]:
+                out[i] += odd
+            else:
+                out[i] += even
+        return out
+
+    def oddEvenCount(self, root, par, color, colors, g):
+        odd, even = 0, 1
+        colors[root] = color
+        for cur in g[root]:
+            if cur == par:
+                continue
+            a, b = self.oddEvenCount(cur, root, color ^ 1, colors, g)
+            odd += b
+            even += a
+        return odd, even
