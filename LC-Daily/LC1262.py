4d9ef88d64a7ed1419e5858c5134527d0d8b4146
Gourav Singh <58901846+gourav2001k@users.noreply.github.com>
2025-11-23 16:04:49 +0530

    LC-Daily 23/11/25

---
+++ LC-Daily/LC1262.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/greatest-sum-divisible-by-three/description/?
+
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        s = sum(nums)
+        if not s % 3:
+            return s
+        h1, h2 = [], []
+        for x in nums:
+            if x % 3 == 1:
+                if len(h1) > 1:
+                    heappushpop(h1, x)
+                else:
+                    heappush(h1, x)
+            elif x % 3 == 2:
+                if len(h2) > 1:
+                    heappushpop(h2, x)
+                else:
+                    heappush(h2, x)
+        if s % 3 == 2:
+            h1, h2 = h2, h1
+        x = 10**6
+        if h1:
+            x = min(x, h1[0])
+        if len(h2) > 1:
+            x = min(x, heappop(h2)+heappop(h2))
+        return s-x
